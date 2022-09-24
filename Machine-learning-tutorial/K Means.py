9b8c1cfc3344e2d044822f4cfc9b391aea6767bc
qJay44 <m70155302@yandex.ru>
2022-09-25 01:34:01 +0300

    folder fix

---
+++ Machine-learning-tutorial/K Means.py
@@ -0,0 +1,45 @@
+import numpy as np
+from time import time
+from sklearn.preprocessing import scale
+from sklearn.datasets import load_digits
+from sklearn.cluster import KMeans
+from sklearn import metrics
+
+np.random.seed(42)
+
+digits = load_digits()
+data = scale(digits.data)
+
+n_samples, n_features = data.shape
+labels = digits.target
+n_digits = len(np.unique(labels))
+
+sample_size = 300
+
+print(82 * '_')
+
+print(f'''
+      Digits number: {n_digits},
+      Samples number: {n_samples},
+      Features number: {n_features}
+''')
+
+def bench_k_means(estimator, name, data):
+    t0 = time()
+    estimator.fit(data)
+    print(f'''
+          init: {name:10},
+          time: {time() - t0:.2f},
+          inertia: {int(estimator.inertia_)},
+          homo: {metrics.homogeneity_score(labels, estimator.labels_):.3f},
+          compl: {metrics.completeness_score(labels, estimator.labels_):.3f},
+          v-meas: {metrics.v_measure_score(labels, estimator.labels_):.3f},
+          ARI: {metrics.adjusted_rand_score(labels, estimator.labels_):.3f},
+          AMI: {metrics.adjusted_mutual_info_score(labels, estimator.labels_):.3f},
+          silhouette: {metrics.silhouette_score(data, estimator.labels_):.3f}
+    ''')
+
+clf = KMeans(init='k-means++', n_clusters=n_digits, n_init=10)
+bench_k_means(clf, 'k-means++', data)
+
+print(82 * '_')
---
+++ Machine-learning-tutorial/KNN Working File.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import linear_model, preprocessing
+
+data = pd.read_csv('data/car.data')
+
+le = preprocessing.LabelEncoder()
+buying = le.fit_transform(list(data['buying']))
+maint = le.fit_transform(list(data['maint']))
+door = le.fit_transform(list(data['door']))
+persons = le.fit_transform(list(data['persons']))
+lug_boot = le.fit_transform(list(data['lug_boot']))
+safety = le.fit_transform(list(data['safety']))
+machine_class = le.fit_transform(list(data['machine_class']))
+
+predict = 'class'
+
+x = list(zip(buying, maint, door, persons, lug_boot, safety))
+y = list(machine_class)
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+model = KNeighborsClassifier(n_neighbors=7)
+model.fit(x_train, y_train)
+
+acc = model.score(x_test, y_test)
+print(acc)
+
+predicted = model.predict(x_test)
+names = ['unacc', 'acc', 'good', 'vgood']
+
+for x in range(len(predicted)):
+    print('Predicted: ', names[predicted[x]], 'Data: ', x_test[x], 'Actual: ', names[y_test[x]])
